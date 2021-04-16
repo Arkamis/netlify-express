d31c61f0d82ceac1838f229cd7ecd22dbbad968a
Miguel Antonio Rodriguez <miguelantonio_rodriguez@ucol.mx>
2021-04-16 18:37:28 -0500

    wip(setup): add initial endpoint config
    add express route configuration to use the endpoint

---
+++ src/server.js
@@ -0,0 +1,37 @@
+'use strict';
+const express = require('express');
+const serverless = require('serverless-http');
+const generateOrder = require('./handler/generateOrder');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+
+app.use(bodyParser.json());
+
+
+
+app.post("/:route", (req, res) => {
+  try {
+    const handler = require(`./handlers/${req.params.route}`);
+    if (!handler) {
+      return res.status(404).json({
+        message: `not found`,
+      });
+    }
+    return handler(req, res);
+  } catch (e) {
+    console.error(e);
+    return res.status(500).json({
+      message: `unexpected error occured`,
+    });
+  }
+});
+
+
+app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+
+module.exports = app;
+module.exports.handler = serverless(app);
