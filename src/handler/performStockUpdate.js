6e9d37fc2cd2751f672934de84ddeb9cc5aa12b1
Miguel Antonio Rodriguez <miguelantonio_rodriguez@ucol.mx>
2021-04-16 21:34:59 -0500

    wip(performstockupdate): fix performUpdate req

--- src/handler/performStockUpdate.js
+++ src/handler/performStockUpdate.js
@@ -9,17 +9,24 @@ query getProductInventoryId($cantidad: Int!, $id_producto: uuid!, $codigo_postal
 `;
 
 // execute the parent operation in Hasura
-const execute = async (variables) => {
-  const fetchResponse = await fetch("https://rayo-dev.hasura.app/v1/graphql", {
-    method: "POST",
-    body: JSON.stringify({
-      query: HASURA_OPERATION,
-      variables,
-    }),
-  });
-  const data = await fetchResponse.json();
-  console.log("DEBUG: ", data);
-  return data;
+const execute = async (variables, query) => {
+  console.info('Data fetched')
+  try {
+    const fetchResponse = await fetch("https://rayo-dev.hasura.app/v1/graphql", {
+      method: "POST",
+      body: JSON.stringify({
+        query,
+        variables,
+      }),
+    })
+    const data = await fetchResponse.json();
+    console.log("DEBUG: ", data);
+    return data;
+    
+  } catch (error) {
+    console.error('Error on fetch:', error)
+
+  }
 };
 
 // Request Handler
@@ -30,7 +37,9 @@ module.exports = performStockUpdate = async (req, res) => {
     id_cliente,
     detalle_orden
   } = req.body.input;
-
+  console.info('Request retrive following data:', codigo_postal_destino)
+  console.info('Id cliente:', id_cliente)
+  console.info("Detalle Orden: ", detalle_orden);
   try {
     // validateAvailableStock and retrive Inventory ID of that stock.
     const detalle_updated = await retriveInventoryId(detalle_orden, codigo_postal_destino, id_cliente);
@@ -48,6 +57,7 @@ module.exports = performStockUpdate = async (req, res) => {
     
     if (error) {
       // if Hasura operation errors, then throw error
+      console.error(error)
       return res.status(400).json(error[0]);
     }
   }
@@ -61,32 +71,71 @@ module.exports = performStockUpdate = async (req, res) => {
 const retriveInventoryId = async (detalle_orden, destination_zipcode, id_cliente) => {
   //[{cantidad, id_producto}] = detalle_orden
   //return [{cantidad, id_producto, id_inventario}]
-  const HASURA_QUERY_INVENTORY = `
-  query getProductInventoryId($cantidad: Int!, $id_producto: uuid!, $codigo_postal: String, $id_cliente: uuid = "") {
-    inventario(limit: 1, order_by: {disponibles: desc}, where: {disponibles: {_gte: $cantidad}, _and: {almacenamiento: {almacen: {codigo_postal_almacens: {codigo_postal: {_eq: $codigo_postal}}}}}, producto: {id_producto: {_eq: $id_producto}, _and: {id_cliente: {_eq: $id_cliente}}}}) {
-      id_inventario
-    }
+const HASURA_QUERY_INVENTORY = `
+query getProductInventoryId($cantidad: Int!, $id_producto: uuid!, $codigo_postal: String!, $id_cliente: uuid!) {
+  inventario(limit: 1, order_by: {disponibles: desc}, where: {disponibles: {_gte: $cantidad}, _and: {almacenamiento: {almacen: {codigo_postal_almacens: {codigo_postal: {_eq: $codigo_postal}}}}}, producto: {id_producto: {_eq: $id_producto}, _and: {id_cliente: {_eq: $id_cliente}}}}) {
+    id_inventario
   }
-  `;
-  const detalle_products_with_inventories_by_pk = await Promise.all(detalle_orden.map( async detalle => {
-    
-    const { cantidad, id_producto } = detalle;
+}
+`;
 
-    const { data: inventory_response, errors } = await execute({
-      cantidad,
-      id_producto,
-      codigo_postal: destination_zipcode,
-      id_cliente
-    }, HASURA_QUERY_INVENTORY);
-    const inventory_by_pk = inventory_response.inventario[0];
-    if (erros) {
-      console.error(errors)
-      throw new Error(errors)
+  const updated_detalle_with_inventory = [];
+  // const detalle_products_with_inventories_by_pk = await Promise.all(detalle_orden.map( async detalle => {
+    
+  //   const { cantidad, id_producto } = detalle;
+  //   console.info(
+  //     `\nQuery for data: cantidad ${cantidad} id producto: ${id_producto}`
+  //   );
+
+  //   const { data: inventory_response, errors } = await execute({
+  //     cantidad,
+  //     id_producto,
+  //     codigo_postal: destination_zipcode,
+  //     id_cliente
+  //   }, HASURA_QUERY_INVENTORY);
+
+  //   console.info(
+  //     `\nGot FRom query: inventary_response: ${inventory_response}`
+  //   );
+  //   const inventory_by_pk = inventory_response.inventario[0];
+
+  //   if (errors) {
+  //     console.error("Error getting Inventories:", errors[0])
+  //     throw new Error(errors)
+  //   }
+  //   if (inventory_by_pk) {
+  //     console.info('Got:', inventory_by_pk)
+  //     return { inventory_by_pk, ...detalle }
+  //   }
+  // }));
+ for (const detalle of detalle_orden) {
+    const { cantidad, id_producto } = detalle;
+    console.info(
+      `\nQuery for data: cantidad ${cantidad} id producto: ${id_producto}`
+    );
+
+    const { data, errors } = await execute(
+      {
+        cantidad,
+        id_producto,
+        codigo_postal: destination_zipcode,
+        id_cliente,
+      },
+      HASURA_QUERY_INVENTORY
+    );
+
+    // console.info(`\nGot FRom query: inventary_response: ${inventory_response}`);
+    const inventory_by_pk = data.inventario[0];
+
+    if (errors) {
+      console.error("Error getting Inventories:", errors[0]);
+      throw new Error(errors);
     }
     if (data) {
-      return { inventory_by_pk, ...detalle }
+      console.info("Got:", inventory_by_pk);
+      updated_detalle_with_inventory.push({ inventory_response, ...detalle });
     }
-  }));
+ }
 
   return detalle_products_with_inventories_by_pk;
 }
--- src/server.js
+++ src/server.js
@@ -9,29 +9,29 @@ const path = require('path');
 const app = express();
 
 
-app.use(bodyParser.json());
 
 const router = express.Router()
 
-router.get("/:route", (req, res) => {
-  try {
-    const handler = require(`./handler/${req.params.route}`);
-    if (!handler) {
-      return res.status(404).json({
-        message: `not found`,
-      });
-    }
-    return handler(req, res);
-  } catch (e) {
-    console.error(e);
-    return res.status(500).json({
-      message: `unexpected error occured`,
-    });
-  }
-});
+// router.get("/:route", (req, res) => {
+//   try {
+//     const handler = require(`./handler/${req.params.route}`);
+//     if (!handler) {
+//       return res.status(404).json({
+//         message: `not found`,
+//       });
+//     }
+//     return handler(req, res);
+//   } catch (e) {
+//     console.error(e);
+//     return res.status(500).json({
+//       message: `unexpected error occured`,
+//     });
+//   }
+// });
 
 router.post("/performStockUpdate", performStockUpdate);
 
+app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
